5ef47b01840248914d8cbf4ea8081c26e8e21363
Shane Yuan <shaneyuan18@gmail.com>
2018-06-13 14:43:52 -0700

    Finish Queue functional

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,15 +5,25 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
-
+  var head = 0;
+  var end = 0;
   someInstance.enqueue = function(value) {
+    end++;
+    someInstance.storage[end.toString()] = value;
+	return value;
   };
 
   someInstance.dequeue = function() {
+    head++;
+	var result = someInstance.storage[head.toString()];
+    delete someInstance.storage[head.toString()];
+	return result;
   };
 
   someInstance.size = function() {
+    return Object.keys(someInstance.storage).length;
   };
+  someInstance.storage = storage;
 
   return someInstance;
 };
