6f8e7fa7e226e84f138e347587a3f690988455c9
choiwa <joyjoycho@gmail.com>
2018-06-14 15:39:53 -0700

    finished linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,13 +3,41 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
+  // if first number to the list, we need to add the head and tail
   list.addToTail = function(value) {
+    // if first number to the list, we need to add the head and tail
+    var curNode = Node(value);
+
+    // if the list is empty
+    if (list.head === null) {
+      list.head = curNode;
+    } else {
+      // if the list has nodes
+      var temp = list.tail;
+      temp.next = curNode;
+    }
+    //node adds to tail
+    list.tail = curNode;
   };
 
   list.removeHead = function() {
+    var temp = list.head.value;
+    list.head = list.head.next;
+
+    return temp;
   };
 
   list.contains = function(target) {
+    var temp = list.head;
+
+    while(temp !== null) {
+      if (temp.value === target) {
+        return true;
+      } else {
+        temp = temp.next;
+      }
+    }
+    return false;
   };
 
   return list;
