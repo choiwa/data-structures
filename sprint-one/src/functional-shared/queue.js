c9b9b79c3dd7034afac0862f18536a67b72b9593
Joyce Cho <joyjoycho@gmail.com>
2018-06-13 21:23:30 -0700

    completed functional-shared queue and stack

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,39 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.count = 0;
+  someInstance.storage = {};
+
+  for (var key in queueMethods) {
+    someInstance[key] = queueMethods[key];
+  }
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  push: function() {
 
+  },
+  size: function() {
+    return this.count < 0 ? 0 : this.count;
+  },
+  enqueue: function(val) {
+    this.storage[this.count] = val;
+    this.count++;
+    return val;
+  },
+  dequeue: function() {
+    var temp = this.storage[0];
+    delete this.storage[0];
+    this.count--;
 
+    for (var key in this.storage) {
+      for (var i = 0; i <= this.count; i++) {
+        this.storage[i] = this.storage[key];
+      }
+    }
+
+    return temp;
+  }
+};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,37 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.count = 0;
+  someInstance.storage = {};
+
+  for (var key in stackMethods) {
+    someInstance[key] = stackMethods[key];
+  }
+  console.log(someInstance);
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
 
+  push: function(val) {
+    this.count++;
+    this.storage[this.count] = val;
+    return this.storage[this.count];
+  },
+  pop: function() {
+    this.count--;
+    var temp = this.storage[this.count + 1];
+    delete this.storage[this.count + 1];
+    return temp;
 
+  },
+  size: function() {
+    return this.count < 0 ? 0 : this.count;
+  }
+
+};
+
+// var q = Stack();
+// q// functions saved
+// q.size();
