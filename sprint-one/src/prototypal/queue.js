b13d6f9037f2cd57cc39eed2f5094d1892b6650e
Shane Yuan <shaneyuan18@gmail.com>
2018-06-13 18:13:23 -0700

    fix syntax and naming for prototypal queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue.head = 0;
+  newQueue.end = 0;
+  newQueue.storage = {};
+  return newQueue;
 };
 
 var queueMethods = {};
+
+queueMethods.size = function() {
+  return Object.keys(this.storage).length;
+}
+
+queueMethods.enqueue = function(value) {
+  this.end++;
+  this.storage[this.end.toString()] = value;
+}
+
+queueMethods.dequeue = function() {
+  this.head++;
+  var removeVal = this.storage[this.head.toString()];
+  delete this.storage[this.head.toString()];
+  return removeVal;
+}
 
 
