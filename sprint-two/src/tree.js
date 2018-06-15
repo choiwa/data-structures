5a7a95aadf4b8e9c4ff782f3f1875f2166cd2a45
choiwa <joyjoycho@gmail.com>
2018-06-14 22:57:29 -0700

    finished tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,30 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+
+  var found = false;
+  if (this.value === target) {
+    return true;
+  }
+
+  for(var i = 0; i < this.children.length; i++) {
+    var child = this.children[i];
+    found = found || child.contains(target);
+  }
+  return found;
 };
 
 
